925092adcf6d6ec3e8ad05d3d56120633504b82a
Dedan Opiyo <dedan.opiyo@student.moringaschool.com>
2025-05-24 11:47:58 +0300

    Migrations and Alembic. Manage database tables and schemas without ever writing SQL through Alembic.

---
+++ app/migrations/versions/9a2f2097a86d_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 9a2f2097a86d
+Revises: 
+Create Date: 2025-05-24 10:50:42.835517
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9a2f2097a86d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
